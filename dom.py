65409e2876e826e9e77fbbb8a78dc8245f74f0c5
goodfella-afk <57734294+goodfella-afk@users.noreply.github.com>
2022-10-24 11:41:09 +0200

    Update dom.py

--- dom.py
+++ dom.py
@@ -1,35 +1,51 @@
 # deklarisemo graf u vidu dictionarija
-# tako da svakom kljucu(cvoru, tj. nodi) dodijelimo-pokazemo value-susjedne cvorove koje vidi.
+# tako da svakom kljucu(cvoru, tj. nodi) dodijelimo-pokazemo SAMO value-susjedne cvorove koje vidi.
 
 graph = {
-    'S': ['D','A'],    # S vidi A i D
-    'D': ['E','B'],    # etc
-    'A': ['B','D'],      
-    'E': ['F'],
-    'B': ['C'],
-    'F': ['G'],
-    'G': [],
-    'C': []
+
+    'S': ['A','D'],    # S vidi A i D
+    'D': ['E','S','B','A'],    # etc
+    'A': ['B','S','D'],      
+    'E': ['F','D'],
+    'B': ['C','A','D'],
+    'F': ['G','E'],
+    'G': ['F'],
+    'C': ['B']
 }
+
 
 print ('\n')
 
-visited = set() # visited nodes, keep track 
+visited = [] # visited nodes, keep track 
 
-def df(visited, graph, node):  # definisemo funkciju a za argumente uzimamo posjecene cvorove,
-                               #graf, i cvor u kom se trenutno nalazimo
-    if node not in visited:
+def df(visited, graph, node):  # definisemo funkciju a za argumente uzimamo promjenjivu visited(listu), sam graf i promjenivu node koju cemo kasnije rekurzivno prosledjivati sa stacka u vidu komsija.
+                               
+    if node not in visited:     # ako cvor koji provjeravamo vec nije posjecen, onda ga stampamo i smjestamo u visited.
         print (f'{node} -> I SEE {graph.get(node)} -> putting {node} in visited')
-        visited.add(node)                                                           # u visited dodajemo trenutni cvor
-        print (f'VISITED:{visited}\n')
-        if node == 'G':                                                             # ovo je trenutno dok ne smislimo kako prekinuti loop kad nadje G
-            print('G FOUNDED, RETURNING IN PARENT NODE\n')
+        visited.append(node)                                                       # u visited dodajemo trenutni cvor
+        print (f'VISITED:{visited}\n')          #vizualizacija liste.
         
-        for neighbour in graph[node]:                                       # udji u prvi sledeci cvor koji trenutni cvor vidi:
-            if neighbour not in visited:                                    # ako taj cvor nismo ranije posjetili rekurzivno pozovi df.
+        for neighbour in graph[node]:                        # udji u prvi sledeci cvor koji trenutni cvor vidi (komsiju, a ostale koje vidis stavi na stack):
+            if neighbour not in visited:                     # ako taj cvor nismo ranije posjetili rekurzivno pozovi df a ako jesmo vrati se na pocetak steka i nastavi dalje.
                 df(visited, graph, neighbour)
 
 
+# main
 
-# call
-df(visited, graph, 'S')
+print ('Unesite startni cvor: S,D,A,E,B,F,G,C: ')                
+x = input('')                                                          # u x smijestamo cvor iz kog zelimo da pocnemo pretragu grafa, takodje ovaj x proosledjujemo kao argument funkciji za pocetnu nodu.
+print ('Unesite cvor do kod zelite da stignete: S,D,A,E,B,F,G,C: ')
+y = input('')                                                          # y koristimo kao zeljeni krajnji cvor.
+df(visited, graph, x)
+
+print ('  ||')
+print ('  ||')
+print ('  ||')
+print ('  \/\n')
+
+for node in visited:                                # provjera liste da vidimo u kom pravcu je graf posjecen + "graficki" prikaz 
+    if node != y:
+        print ('  ', node)
+    else:
+        break
+print ('  ', y, '    KRAJ')               
